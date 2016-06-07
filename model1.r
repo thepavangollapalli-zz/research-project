ad58a217067d63a50acbef4863b4cfa280875f6b
Pavan Gollapalli <pavankgollapalli@gmail.com>
2016-06-07 14:26:38 -0400

    testing with several ratios of alpha/beta and mu/x

--- model1.r
+++ model1.r
@@ -6,10 +6,239 @@
 #alpha, beta changed values (now 0.9 and 0.5 respectively)
 
 n = 10^4
+c = 250
+print("mu/x: 1/1")
+mu = 500
+x = 500
+
+print("0.9, 0.5")
 alpha = rnorm(n, mean=0.9, sd=0.1)
 beta = rnorm(n, mean=0.5, sd=0.1)
-mu = 500
-c = 250
+Pn = alpha * (1 - beta) * mu - beta * x - c
+sum(Pn) / n
+
+print("0.9, 0.25")
+n = 10^4
+alpha = rnorm(n, mean=0.9, sd=0.1)
+beta = rnorm(n, mean=0.25, sd=0.1)
+Pn = alpha * (1 - beta) * mu - beta * x - c
+sum(Pn) / n
+
+print("0.9, 0.1")
+n = 10^4
+alpha = rnorm(n, mean=0.9, sd=0.1)
+beta = rnorm(n, mean=0.1, sd=0.1)
+Pn = alpha * (1 - beta) * mu - beta * x - c
+sum(Pn) / n
+
+print("0.5, 0.5")
+n = 10^4
+alpha = rnorm(n, mean=0.5, sd=0.1)
+beta = rnorm(n, mean=0.5, sd=0.1)
+Pn = alpha * (1 - beta) * mu - beta * x - c
+sum(Pn) / n
+
+print("0.5, 0.25")
+n = 10^4
+alpha = rnorm(n, mean=0.5, sd=0.1)
+beta = rnorm(n, mean=0.25, sd=0.1)
+Pn = alpha * (1 - beta) * mu - beta * x - c
+sum(Pn) / n
+
+print("0.5, 0.1")
+n = 10^4
+alpha = rnorm(n, mean=0.5, sd=0.1)
+beta = rnorm(n, mean=0.1, sd=0.1)
+Pn = alpha * (1 - beta) * mu - beta * x - c
+sum(Pn) / n
+
+print("0.25, 0.25")
+n = 10^4
+alpha = rnorm(n, mean=0.25, sd=0.1)
+beta = rnorm(n, mean=0.25, sd=0.1)
+Pn = alpha * (1 - beta) * mu - beta * x - c
+sum(Pn) / n
+
+print("0.25, 0.1")
+n = 10^4
+alpha = rnorm(n, mean=0.25, sd=0.1)
+beta = rnorm(n, mean=0.1, sd=0.1)
+Pn = alpha * (1 - beta) * mu - beta * x - c
+sum(Pn) / n
+
+print("mu/x: 2/1")
+mu = 1000
 x = 500
+
+print("0.9, 0.5")
+alpha = rnorm(n, mean=0.9, sd=0.1)
+beta = rnorm(n, mean=0.5, sd=0.1)
+Pn = alpha * (1 - beta) * mu - beta * x - c
+sum(Pn) / n
+
+print("0.9, 0.25")
+n = 10^4
+alpha = rnorm(n, mean=0.9, sd=0.1)
+beta = rnorm(n, mean=0.25, sd=0.1)
+Pn = alpha * (1 - beta) * mu - beta * x - c
+sum(Pn) / n
+
+print("0.9, 0.1")
+n = 10^4
+alpha = rnorm(n, mean=0.9, sd=0.1)
+beta = rnorm(n, mean=0.1, sd=0.1)
+Pn = alpha * (1 - beta) * mu - beta * x - c
+sum(Pn) / n
+
+print("0.5, 0.5")
+n = 10^4
+alpha = rnorm(n, mean=0.5, sd=0.1)
+beta = rnorm(n, mean=0.5, sd=0.1)
+Pn = alpha * (1 - beta) * mu - beta * x - c
+sum(Pn) / n
+
+print("0.5, 0.25")
+n = 10^4
+alpha = rnorm(n, mean=0.5, sd=0.1)
+beta = rnorm(n, mean=0.25, sd=0.1)
+Pn = alpha * (1 - beta) * mu - beta * x - c
+sum(Pn) / n
+
+print("0.5, 0.1")
+n = 10^4
+alpha = rnorm(n, mean=0.5, sd=0.1)
+beta = rnorm(n, mean=0.1, sd=0.1)
+Pn = alpha * (1 - beta) * mu - beta * x - c
+sum(Pn) / n
+
+print("0.25, 0.25")
+n = 10^4
+alpha = rnorm(n, mean=0.25, sd=0.1)
+beta = rnorm(n, mean=0.25, sd=0.1)
+Pn = alpha * (1 - beta) * mu - beta * x - c
+sum(Pn) / n
+
+print("0.25, 0.1")
+n = 10^4
+alpha = rnorm(n, mean=0.25, sd=0.1)
+beta = rnorm(n, mean=0.1, sd=0.1)
+Pn = alpha * (1 - beta) * mu - beta * x - c
+sum(Pn) / n
+
+print("mu/x: 4/1")
+mu = 2000
+x = 500
+
+print("0.9, 0.5")
+alpha = rnorm(n, mean=0.9, sd=0.1)
+beta = rnorm(n, mean=0.5, sd=0.1)
+Pn = alpha * (1 - beta) * mu - beta * x - c
+sum(Pn) / n
+
+print("0.9, 0.25")
+n = 10^4
+alpha = rnorm(n, mean=0.9, sd=0.1)
+beta = rnorm(n, mean=0.25, sd=0.1)
+Pn = alpha * (1 - beta) * mu - beta * x - c
+sum(Pn) / n
+
+print("0.9, 0.1")
+n = 10^4
+alpha = rnorm(n, mean=0.9, sd=0.1)
+beta = rnorm(n, mean=0.1, sd=0.1)
+Pn = alpha * (1 - beta) * mu - beta * x - c
+sum(Pn) / n
+
+print("0.5, 0.5")
+n = 10^4
+alpha = rnorm(n, mean=0.5, sd=0.1)
+beta = rnorm(n, mean=0.5, sd=0.1)
+Pn = alpha * (1 - beta) * mu - beta * x - c
+sum(Pn) / n
+
+print("0.5, 0.25")
+n = 10^4
+alpha = rnorm(n, mean=0.5, sd=0.1)
+beta = rnorm(n, mean=0.25, sd=0.1)
+Pn = alpha * (1 - beta) * mu - beta * x - c
+sum(Pn) / n
+
+print("0.5, 0.1")
+n = 10^4
+alpha = rnorm(n, mean=0.5, sd=0.1)
+beta = rnorm(n, mean=0.1, sd=0.1)
+Pn = alpha * (1 - beta) * mu - beta * x - c
+sum(Pn) / n
+
+print("0.25, 0.25")
+n = 10^4
+alpha = rnorm(n, mean=0.25, sd=0.1)
+beta = rnorm(n, mean=0.25, sd=0.1)
+Pn = alpha * (1 - beta) * mu - beta * x - c
+sum(Pn) / n
+
+print("0.25, 0.1")
+n = 10^4
+alpha = rnorm(n, mean=0.25, sd=0.1)
+beta = rnorm(n, mean=0.1, sd=0.1)
+Pn = alpha * (1 - beta) * mu - beta * x - c
+sum(Pn) / n
+
+print("mu/x: 8/1")
+mu = 4000
+x = 500
+
+print("0.9, 0.5")
+alpha = rnorm(n, mean=0.9, sd=0.1)
+beta = rnorm(n, mean=0.5, sd=0.1)
+Pn = alpha * (1 - beta) * mu - beta * x - c
+sum(Pn) / n
+
+print("0.9, 0.25")
+n = 10^4
+alpha = rnorm(n, mean=0.9, sd=0.1)
+beta = rnorm(n, mean=0.25, sd=0.1)
+Pn = alpha * (1 - beta) * mu - beta * x - c
+sum(Pn) / n
+
+print("0.9, 0.1")
+n = 10^4
+alpha = rnorm(n, mean=0.9, sd=0.1)
+beta = rnorm(n, mean=0.1, sd=0.1)
+Pn = alpha * (1 - beta) * mu - beta * x - c
+sum(Pn) / n
+
+print("0.5, 0.5")
+n = 10^4
+alpha = rnorm(n, mean=0.5, sd=0.1)
+beta = rnorm(n, mean=0.5, sd=0.1)
+Pn = alpha * (1 - beta) * mu - beta * x - c
+sum(Pn) / n
+
+print("0.5, 0.25")
+n = 10^4
+alpha = rnorm(n, mean=0.5, sd=0.1)
+beta = rnorm(n, mean=0.25, sd=0.1)
+Pn = alpha * (1 - beta) * mu - beta * x - c
+sum(Pn) / n
+
+print("0.5, 0.1")
+n = 10^4
+alpha = rnorm(n, mean=0.5, sd=0.1)
+beta = rnorm(n, mean=0.1, sd=0.1)
+Pn = alpha * (1 - beta) * mu - beta * x - c
+sum(Pn) / n
+
+print("0.25, 0.25")
+n = 10^4
+alpha = rnorm(n, mean=0.25, sd=0.1)
+beta = rnorm(n, mean=0.25, sd=0.1)
+Pn = alpha * (1 - beta) * mu - beta * x - c
+sum(Pn) / n
+
+print("0.25, 0.1")
+n = 10^4
+alpha = rnorm(n, mean=0.25, sd=0.1)
+beta = rnorm(n, mean=0.1, sd=0.1)
 Pn = alpha * (1 - beta) * mu - beta * x - c
 sum(Pn) / n
